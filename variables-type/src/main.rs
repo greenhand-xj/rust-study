50adab01a16f38e9343db9ddb2cd34929bcf4048
徐杰 <2275817182@qq.com>
2025-04-08 23:20:12 +0800

    feat: 基本类型学习 类型推断很nb啊

---
+++ variables-type/src/main.rs
@@ -0,0 +1,46 @@
+fn main() {
+    // let mut x = 4;
+    // println!("this values is {}", x);
+    // x = 5;
+    // println!("this values is {}", x);
+
+    // const
+    // const WO_CAO: i32 = 54;
+
+
+    //shadowing
+    // let x = 5;
+    // let x = x + 1;
+    // {
+    //     let x = x * 2;
+    //     println!("this inner x value is {x}");
+    // }
+    //
+    //
+    // println!("this outer x value is {x}");
+
+    // let a = 32;
+    // let b: u32 = 42;
+    // let c: u8 = 255;
+    // let d: usize = 46;
+    // let e: i32 = 0xff;
+    // let f = b'A';
+    // let c = '我';
+    // let w  = '👇';
+    //
+    // println!("{c} ==== {w}");
+
+    // let tup = (100,'a',3.0);
+    // let (_,y,z) = tup;
+    // let a = [3;4];
+
+    // let a: i32 = 10;
+    // let b: u32 = 20;
+    // let c = a + b;
+    // println!("a: {a},b: {b}");
+
+    const THREE_AND_A_BIT: f64 = 3.4028236;
+
+    println!("{}", THREE_AND_A_BIT);
+}
+
