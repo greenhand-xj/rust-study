8defef3a77e9e0e0ca1761c8d0b82aa9496918f8
徐杰 <2275817182@qq.com>
2025-04-10 23:33:45 +0800

    feat: owership

---
+++ ownership/src/main.rs
@@ -0,0 +1,39 @@
+fn main() {
+    // let first = String::from("Ferris");
+    // let full = add_suffix(first);
+    // moved
+    // println!("{}", full);
+    // println!("{}", first);
+    // let mut a = Box::new(16);
+    // let mut b = a;
+    // let c = test(b);
+    //
+    // println!("{}",a);
+    // let s = String::from("hello");
+    // let s2 = add_suffix(s);
+    // println!("{}", s2);
+
+    // let s = String::from("hello world");
+    // let s2;
+    // let b = false;
+    // // false也能判断出来被 moved,这么严格吗
+    // if b {
+    //     s2 = s;
+    // }
+    // println!("{}", s);
+}
+
+// fn add_suffix(mut s: String) -> String {
+//     s.push_str(" World");
+//     return s;
+// }
+
+// fn test(mut c: Box<i32>) -> Box<i32> {
+//     c = Box::new(8);
+//     c
+// }
+
+// fn add_suffix(mut name: String) -> String {
+//     name.push_str(" Jr.");
+//     return name;
+// }
