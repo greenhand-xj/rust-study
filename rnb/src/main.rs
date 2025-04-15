aef738417e342530486aac533e573e0c0ec63ce7
徐杰 <2275817182@qq.com>
2025-04-15 22:44:35 +0800

    feat: 引用 & 借用

--- rnb/src/main.rs
+++ rnb/src/main.rs
@@ -23,16 +23,54 @@ fn main() {
     // println!("r2: {}", r2);
 
 
+    // let mut v = vec![1,2,3];
+    //
+    // let mut num = &v[2];
+    // let z = 4;
+    // num = &z;
+    // println!("num: {}", *num);
+    // println!("vec: {}", v[2]);
+    //
+    // v.push(4);
+
+    // let mut v = vec![1,2,3]; // RWO
+    // let num = &v[2]; //RO v-W -O
+    // // v.push(4);
+    // println!("{:?}", num);
+    // println!("{:p}", &num);
+    // println!("{:p}", &v[2]);
+
+    // let mut v = vec![1,2,3];
+    // let num = &mut v[2];
+    // // v[2] = 4;
+    // // *num = 5;
+    //
+    // let num2 = &*num;
+    // println!("{} {}", *num2,*num);
+    // let mut n = 1;
+    // incr(&mut n);
+    // println!("{}", n);
+    // let mut s = String::from("hello");
+    // let s2 = &s;
+    // // let s3 = &mut s;
+    // // s3.push_str(", world");
+    // s.push_str(", world");
+    // println!("{}", s2);
+
     let mut v = vec![1,2,3];
-
-    let mut num = &v[2];
-    let z = 4;
-    num = &z;
-    println!("num: {}", *num);
-    println!("vec: {}", v[2]);
-
-    v.push(4);
+    let n = &v[0];
+    give_and_take(&mut v,4);
+    println!("{}",n);
 }
+
+fn give_and_take(v: &mut Vec<i32>,n: i32) -> i32 {
+    v.push(n);
+    v.remove(0)
+}
+
+// fn incr(n: &mut i32){
+//     *n += 1;
+// }
 
 // fn greet(g1: &String, g2: &String)  {
 //     println!("{} {}", g1,g2);
