29fcd0952b8f018f454bc6d373f126238df22da5
徐杰 <2275817182@qq.com>
2025-04-22 22:32:03 +0800

    feat: strings

---
+++ strings/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    // println!("Hello, world!");
+    let s1 = String::from("hello world");
+    let mut s2 = String::from("hello world");
+    // let s3 = s1 + &s2;
+    // s2.push_str(s1);
+    println!("{}", s1);
+}
+
