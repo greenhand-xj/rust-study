761220ddca5fb5d2a95a28b9dde6b095dcf197e1
徐杰 <2275817182@qq.com>
2025-05-11 21:08:52 +0800

    feat: minigrep & closure

---
+++ closures/src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    let f = |_| ();
+
+    let s = String::from("Hello");
+    f(s);
+    // println!("{}",s);
+}
--- mingrep/src/main.rs
+++ mingrep/src/main.rs
@@ -3,9 +3,9 @@ use mingrep::{Config};
 
 fn main() {
     //解析命令行参数
-    let args = env::args().collect::<Vec<String>>();
+    // let args = env::args().collect::<Vec<String>>();
     // dbg!(args);
-    let config = Config::build(&args).unwrap_or_else(|err| {
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments {}", err);
         process::exit(1);
     });
