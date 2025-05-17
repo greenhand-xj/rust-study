9d914dd79415f61fe6ca28b4b2ade6d59f9ac213
徐杰 <2275817182@qq.com>
2025-05-17 21:10:46 +0800

    feat: thread

---
+++ thread/src/main.rs
@@ -0,0 +1,29 @@
+use std::thread;
+use std::time::Duration;
+
+fn main() {
+    // let handle = thread::spawn(|| {
+    //     for i in 1..10 {
+    //         println!("hi number {} from the spawned thread!", i);
+    //         thread::sleep(Duration::from_millis(1));
+    //     }
+    // });
+    //
+    // for i in 1..5 {
+    //     println!("hi number {} from the main thread!", i);
+    //     thread::sleep(Duration::from_millis(1));
+    // }
+    //
+    // handle.join().unwrap();
+
+    let mut n = 1;
+    let t = thread::spawn(move || {
+        n = n + 1;
+        thread::spawn(move || {
+            n = n + 1;
+        })
+    });
+    n = n +1;
+    t.join().unwrap().join().unwrap();
+    println!("{}", n);
+}
